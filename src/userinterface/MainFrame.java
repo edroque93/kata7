f4e184298d708479ebf0ee395edcbe016d36e33a
Quique <Quique@10.230.169.43>
2013-11-08 11:16:47 +0000

    Primera pausa de clase

---
+++ src/userinterface/MainFrame.java
@@ -0,0 +1,64 @@
+package userinterface;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame() throws HeadlessException {
+        this.setTitle("MoneyCalculator");
+        this.setMinimumSize(new Dimension(300, 300));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        createComponents();
+        this.setVisible(true);
+    }
+
+    private void createComponents() {
+        this.add(createToolbar(), BorderLayout.SOUTH);
+    }
+
+    private JPanel createToolbar() {
+        JPanel toolbar = new JPanel();
+        toolbar.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        toolbar.add(createCalculateButton());
+        toolbar.add(createExitButton());
+        return toolbar;
+    }
+
+    private JButton createExitButton() {
+        JButton button = new JButton("Exit");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                exit();
+            }
+        });
+        return button;
+    }
+
+    private JButton createCalculateButton() {
+        JButton button = new JButton("Calculate");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                calculate();
+            }
+        });
+        return button;
+    }
+
+    private void exit() {
+        dispose();
+    }
+
+    private void calculate() {
+        System.out.println("Calculating...");
+    }
+}
---
+++ src/userinterface/UserInterface.java
@@ -0,0 +1,8 @@
+package userinterface;
+
+public class UserInterface {
+
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+    }
+}
